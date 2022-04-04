bf7ad0cdfa272ba639752d4e175f3f8fcd416e11
Paul Ludwigs <paul.ludwigs@web.de>
2022-04-04 10:31:45 +0200

    Add second player

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,13 +1,26 @@
 const handSigns = ['rock', 'paper', 'scissors'];
-const names = ['Darcey', 'Morris', 'Drew', 'Homer', 'Ruby', 'Ronnie', 'Mateusz', 'Cloe', 'Vera'];
+let names = ['Darcey', 'Morris', 'Drew', 'Homer', 'Ruby', 'Ronnie', 'Mateusz', 'Cloe', 'Vera'];
 
 function randomElement(array)
 {
-    return Math.floor(Math.random * array.length);
+    return Math.floor(Math.random() * array.length);
 }
+
 
 function randomGame()
 {
-    const handSign = handSigns[randomElement(handSigns)];
-    const name = names[randomElement(names)];
+    const handSign1 = handSigns[randomElement(handSigns)];
+    const name1 = names[randomElement(names)];
+    names = names.filter(name => name !== name1);
+    const handSign2 = handSigns[randomElement(handSigns)];
+    const name2 = names[randomElement(names)];
+
+    console.log(names);
+
+    console.log(`${name1} and ${name2} are playing a game of rock, paper, scissors`);
+    console.log(`${name1} is picking ${handSign1}`);
+    console.log(`${name2} is picking ${handSign2}`);
+
 }
+
+randomGame();
